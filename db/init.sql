20fcb0ff5e88139cab2a1eceda95566e77503c2f
Andrew Mason <andrew@nocturnal.net.au>
2018-07-25 12:12:30 +0930

    Set required fields as NOT NULL and add default values for the created date

--- db/init.sql
+++ db/init.sql
@@ -5,12 +5,12 @@ DROP TABLE IF EXISTS `oxipay_vend_map`;
 --
 create table oxipay_vend_map (
     id int NOT NULL  auto_increment,
-    fxl_register_id  text,
-    fxl_seller_id text,
+    fxl_register_id text NOT NULL,
+    fxl_seller_id text NOT NULL,
     fxl_device_signing_key text,
     origin_domain text NOT NULL ,
-    vend_register_id text,
-    created_date datetime,
+    vend_register_id text NOT NULL,
+    created_date datetime DEFAULT CURRENT_TIMESTAMP,
     created_by text NOT NULL ,
     modified_date datetime,
     modified_by text,
@@ -31,7 +31,7 @@ INSERT INTO oxipay_vend_map (
     'Oxipos',
     '30188105',
     'JCjbPGtuniWr',
-    'https://demo.oxipay.com.au',
+    'https://sand.oxipay.com.au',
     '57d863b4-4ae0-492c-b44a-326db76f7dac',
     'andrewm'
 );
