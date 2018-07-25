6c1b7114a05dfcf7d4369b7a14537c198f4afffc
Andrew Mason <andrew@nocturnal.net.au>
2018-07-25 12:35:41 +0930

    Add a test for saving the terminal

--- main_test.go
+++ main_test.go
@@ -8,6 +8,25 @@ import (
 	"testing"
 )
 
+// TestTerminalSave tests saving a new terminal in the database for the registration phase
+func TestTerminalSave(t *testing.T) {
+	// { Success SCRK01 Success VK5NGgc7nFJp 481f1e4098465f5229b33d91e0687c6123b91078e5c727b6d8ebf9360af145e7}
+
+	connectToDatabase()
+
+	terminal := &Terminal{
+		FxlDeviceSigningKey: "VK5NGgc7nFJp",
+		FxlRegisterID:       "Oxipos",
+		Origin:              "http://pos.oxipay.com.au",
+		VendRegisterID:      "VendDevice01",
+	}
+	saved, err := terminal.save("andrewm")
+
+	if err != nil && saved == true {
+		t.Fatal(err)
+	}
+}
+
 // TestGeneratePayload  generating oxipay payload
 func TestRegisterHandler(t *testing.T) {
 
@@ -19,7 +38,7 @@ func TestRegisterHandler(t *testing.T) {
 	form.Add("FirmwareVersion", "1.0")
 	form.Add("TerminalID", "1234")
 	form.Add("DeviceID", "VendDevice01")
-	form.Add("DeviceToken", "Q8dH7V9rRLXs")
+	form.Add("DeviceToken", "q3Cn2c55mDzl")
 	form.Add("OperatorID", "Vend")
 
 	req, err := http.NewRequest(http.MethodPost, "/register", strings.NewReader(form.Encode()))
@@ -43,11 +62,11 @@ func TestRegisterHandler(t *testing.T) {
 	}
 
 	// Check the response body is what we expect.
-	// expected := `{"alive": true}`
-	// if rr.Body.String() != expected {
-	// 	t.Errorf("handler returned unexpected body: got %v want %v",
-	// 		rr.Body.String(), expected)
-	// }
+	// expected := `{ Success SCRK01 Success VK5NGgc7nFJp 481f1e4098465f5229b33d91e0687c6123b91078e5c727b6d8ebf9360af145e7}`
+	//  if rr.Body != expected {
+	//  	t.Errorf("handler returned unexpected body: got %v want %v",
+	//  		rr.Body.String(), expected)
+	//  }
 }
 
 func TestGeneratePayload(t *testing.T) {
