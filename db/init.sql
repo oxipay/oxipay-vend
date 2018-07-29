0b0574d10644f047a6c6affaa141794f3ca3d7bf
Andrew Mason <andrew@nocturnal.net.au>
2018-07-30 00:13:59 +0930

    include session definition

--- db/init.sql
+++ db/init.sql
@@ -43,9 +43,8 @@ DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE sessions (
 	id INT NOT NULL AUTO_INCREMENT,
 	session_data LONGBLOB,
-    created_on TIMESTAMP DEFAULT NOW(),,
-	modifiedOn datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_on TIMESTAMP DEFAULT NOW(),
 	modified_on TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE CURRENT_TIMESTAMP,
     expires_on TIMESTAMP DEFAULT NOW(),
-     PRIMARY KEY(`id`)) 
- ) engine=InnoDB COMMENT = "This stores http sessions and is required by the session store handler";
+     PRIMARY KEY(`id`)
+ ) engine=InnoDB, COMMENT = 'This stores http sessions and is required by the session store handler';
