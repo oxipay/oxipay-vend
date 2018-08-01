9e990fae5229d0e7de9960fdbab3e4fd02b4d992
Andrew M <andrew.mason@oxipay.com.au>
2018-08-01 21:14:49 +0930

    remove old files

--- vendproxy/vendproxy/vend/vend.go
+++
@@ -1,9 +0,0 @@
-package vend
-
-// VendPaymentRequest is the originating request from vend
-type VendPaymentRequest struct {
-	Amount     string
-	Origin     string
-	RegisterID string
-	code       string
-}
