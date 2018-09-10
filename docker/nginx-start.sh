64a4454a88803381d191cace429e782ffdf5b1e6
Andrew Mason <andrew.mason@oxipay.com.au>
2018-09-10 16:50:16 +0930

    use gotemplate in the container

--- docker/nginx-start.sh
+++ docker/nginx-start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-envsubst <  /etc/nginx/sites-available/default.conf > /etc/nginx/sites-enabled/default   
+/usr/local/bin/gotemplate  -f /etc/nginx/sites-available/default > /etc/nginx/sites-enabled/default 
+
 nginx -g "daemon off;"
